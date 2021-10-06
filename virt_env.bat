f87509b1411ed15ecbc9e50e7ecf64ffb13949ae
Maryan Gelyk <maryangelyk16@gmail.com>
2021-10-06 15:43:48 -0400

    Added .bat file

---
+++ virt_env.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+:: This batch file opens activate file and starts virtual enviroment for python
+
+echo "Opening sharm website file"
+cd B:\Python_Projects\Websites\Sharm
+start sharm_env\Scripts\activate.bat
+
+
